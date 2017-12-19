3b3622cac567f33b2387a216f4f53e15e4fe66a2
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2017-12-19 19:01:43 +0100

    person

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,31 +1,38 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
     this.direction = "up";
   }
 
   start() {
-    setInterval(() => this.update(); 1000  )
-  };
-
-  stop() { }
-  update() {
-     this.log();
-    }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() {
-  if((this.floor + 1 ) < this.MAXFLOOR){
-    this.floor += 1;
-  }else console.log("The last floor is the 10th")
+    setInterval(() => this.update, 1000)
   }
-  floorDown() { }
-  call() { }
-  log() {
-     console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
-    }
-}
 
-module.exports = Elevator;
+  stop() {}
+  update() {
+    this.log();
+  }
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {
+    if ((this.floor + 1) < this.MAXFLOOR) {
+      this.floor += 1;
+    } else {
+      console.log("The last floor is the 10th")
+    }
+  }
+  floorDown() {
+    if (this.floor - 1 < 0) {
+      console.log("basement floor")
+    } else {
+      this.floor - 1
+    }
+    call(){}
+    log() {
+      console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+    }
+  }
+
+  module.exports = Elevator;
